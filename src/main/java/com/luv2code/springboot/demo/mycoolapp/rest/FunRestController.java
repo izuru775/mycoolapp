8eeb9373d93d4631ec69e53612014378602f9606
Isuru Adikaram <izuru775@gmail.com>
2025-03-11 15:08:05 +1100

    spring-boot-devtools and Actuator added application.properties added to configure spring-boot-starter-actuator

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,7 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
     // Expose "/" that return "Hello World"
     @GetMapping("/")
-    public String GetString(){
+    public String sayHello(){
         return "Hello World!";
+    }
+
+    // Expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    // Expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day.";
     }
 }
